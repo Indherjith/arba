55e51a29ed4d0fc1758be468fe799f26af24b3e0
Buvaneswaran S <99597201+Indherjith@users.noreply.github.com>
2023-03-31 22:46:34 +0530

    main

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -41,20 +41,20 @@ const login =  async (req, res) => {
     // res.send(req.body)
     const {email, password} = req.body;
     const user = await UserModel.findOne({email})
-    res.send(user)
-    // const hash = user.password
-    // bcrypt.compare(password, hash, function(err, result) {
-    //     if(err){
-    //         res.send("Something went wrong, plz try again later")
-    //     }
-    //     if(result){
-    //         const token = jwt.sign({ userId : user._id }, process.env.JWT_SECRET);
-    //         res.json({message : "Login successfull", token})
-    //     }
-    //     else{
-    //         res.send("Invalid credentials, plz signup if you haven't")
-    //     }
-    // });
+    // res.send(user)
+    const hash = user.password
+    bcrypt.compare(password, hash, function(err, result) {
+        if(err){
+            res.send("Something went wrong, plz try again later")
+        }
+        if(result){
+            const token = jwt.sign({ userId : user._id }, process.env.JWT_SECRET);
+            res.send({message : "Login successfull", token})
+        }
+        else{
+            res.send("Invalid credentials, plz signup if you haven't")
+        }
+    });
     
 }
 
